81dcf58ff2c86b40a87d3266eb412f1cd85ebdde
EmilyWhitaker <ewhitaker524@gmail.com>
2018-08-17 10:59:03 -0500

    working on research page

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,9 +17,16 @@ $email_subject = "Contact form submitted by:  $name";
 $email_body = "You have received a new message. \n\n".
 				  " Here are the details:\n \nName: $name \n ".
 				  "Email: $email_address\n Message: \n $message";
-$headers = "From: ewhitaker524@gmail.com\n"; 
+$headers = "From: ewhitaker524@gmail.com\n";
 // Since this email form will be generated from your server. The From email address will be best using something like this noreply@yourdomain.com
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
 ?>
+
+<!--<form action="https://formspree.io/ewhitaker524@gmail.com"
+      method="POST">
+    <input type="text" name="name">
+    <input type="email" name="_replyto">
+    <input type="submit" value="Send">
+</form>
